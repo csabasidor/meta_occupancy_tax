8c8469a427c34bf0234dcee67bde15a05c3e75fa
cs_sido <csaba.sidor@gmail.com>
2019-01-28 22:56:33 +0100

    Update 01_import_excel.py

--- 01_import_excel.py
+++ 01_import_excel.py
@@ -1,21 +1,15 @@
+#In order for reuse, create own engine
 from sqlalchemy import create_engine, MetaData, Table
 import pandas as pd
 
-
-
-
-exec(open("D:\\2018_Python_testovanie\\csb\\04_engine_gis_zaloha.py").read())
-
-
-
-file = 'D:\\001_CS_publik\\rozrobene\\sustainibility_22022019\\occ_tax_cities.xlsx'
-
+exec(open("path to engine").read())
+file = 'path to file'
 data =  pd.ExcelFile(file)
 print(data.sheet_names)
 df1 = data.parse('abc')
 
 df1.to_sql('city_occ_tax', engine, if_exists='replace')
 
-print(df1[0:6])
+
 
 
